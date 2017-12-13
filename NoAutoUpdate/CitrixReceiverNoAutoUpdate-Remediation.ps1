0f927dba5b2f0e17fca88ec5cf55dac91ceff518
Mieszko Ślusarczyk <mieszkoslusarczyk@gmail.com>
2017-12-13 15:27:48 +0100

    Add CitrixReceiverNoAutoUpdate Remediation script

---
+++ NoAutoUpdate/CitrixReceiverNoAutoUpdate-Remediation.ps1
@@ -0,0 +1,49 @@
+<# 
+   .SYNOPSIS 
+    Checks if automatic update is disabled for Citrix Receiver, and corrects setting if necessary
+
+   .DESCRIPTION
+    Check if registry keys responsible for auto update mechanism are set properly, and corrects them if necessary
+
+   .NOTES
+    AUTHOR: Mieszko Ślusarczyk
+    EMAIL: mieszkoslusarczyk@gmail.com
+    VERSION: 1.0.0.0
+    DATE: 13.12.2017
+    
+    CHANGE LOG: 
+    1.0.0.0 : 13.12.2017  : Initial version of script 
+
+#>
+If (Test-Path "HKLM:\SOFTWARE\Wow6432Node\Citrix\ICA Client\AutoUpdate\Commandline Policy")
+{
+    If  (((Get-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Citrix\ICA Client\AutoUpdate\Commandline Policy\"|Select-Object -ExpandProperty "Banned") -eq "true")`
+        -and`
+        ((Get-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Citrix\ICA Client\AutoUpdate\Commandline Policy\"|Select-Object -ExpandProperty "Enable") -eq "false"))
+        {
+            Write-Host "Compliant"
+        }
+    Else
+    {
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Citrix\ICA Client\AutoUpdate\Commandline Policy\" -Name "Banned" -Value "true"
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Citrix\ICA Client\AutoUpdate\Commandline Policy\" -Name "Enable" -Value "false"
+    }
+}
+ElseIf (Test-Path "HKLM:\SOFTWARE\Citrix\ICA Client\AutoUpdate\Commandline Policy")
+{
+    If  (((Get-ItemProperty "HKLM:\SOFTWARE\Citrix\ICA Client\AutoUpdate\Commandline Policy\"|Select-Object -ExpandProperty "Banned") -eq "true")`
+        -and`
+        ((Get-ItemProperty "HKLM:\SOFTWARE\Citrix\ICA Client\AutoUpdate\Commandline Policy\"|Select-Object -ExpandProperty "Enable") -eq "false"))
+        {
+            Write-Host "Compliant"
+        }
+    Else
+    {
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Citrix\ICA Client\AutoUpdate\Commandline Policy\" -Name "Banned" -Value "true"
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Citrix\ICA Client\AutoUpdate\Commandline Policy\" -Name "Enable" -Value "false"
+    }
+}
+Else
+{
+    Write-Host "Non-Compliant"
+}
